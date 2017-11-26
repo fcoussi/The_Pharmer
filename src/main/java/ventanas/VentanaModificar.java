cc6e59a4db9ce5703060f8e46e9fb6f2eed62d4d
fcoussi <Felipe@172.23.61.241>
2017-11-25 23:12:03 -0300

    "Modificado VentanaModificar"

--- src/main/java/ventanas/VentanaModificar.java
+++ src/main/java/ventanas/VentanaModificar.java
@@ -7,6 +7,13 @@ package ventanas;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import paneles.PanelBoton;
@@ -47,8 +54,7 @@ public class VentanaModificar extends JFrame  {
     String[] nombreBtn ={"Modificar"};
     this.panelSur=new PanelBoton(nombreBtn,5,260);
     this.add(this.panelSur,BorderLayout.SOUTH);
-       
-        
+             
         setSize(615,300);        
         this.setLocationRelativeTo(null);
         this.setResizable(false);
@@ -56,12 +62,53 @@ public class VentanaModificar extends JFrame  {
         setVisible(true);
     }
     
-    public void modificarDatos(String[] datoCelda){
+    public void modificarDatos(String[] datoCelda) throws IOException{    
         
+        String linea=datoCelda[0]+";"+datoCelda[1]+";"+datoCelda[2]+";"+datoCelda[3]+";"+datoCelda[4]+";"+datoCelda[5];
+        String linea2;
         for (int i = 0; i < datoCelda.length; i++) {
-        this.panelCentro.setTxtDatos(i,datoCelda[i]);                
-        }        
-        
+        this.panelCentro.setTxtDatos(i,datoCelda[i]);
+        linea2=linea+datoCelda[i];
+        linea2=linea2+";";                    
+        }
+   
+        String ruta="data\\BaseDeDatos\\"+datoCelda[4]+".txt";
+         
+       // eliminarFila(ruta,linea);        
     }
     
+     public void eliminarFila(String file, String palabraEliminar) throws IOException{
+        try{
+            File enArchivo= new File(file);
+            if (!enArchivo.isFile()) {
+                System.out.println("Parametros no existen dentro archivo");
+                return;
+            }
+            File tempFile= new File(enArchivo.getAbsolutePath());
+          
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+            String linea;
+            
+            while((linea = br.readLine()) !=null){
+                if (!linea.trim().equals(palabraEliminar)) {
+                    pw.println(linea);
+                    pw.flush();
+                }
+            }
+            pw.close();
+            br.close();
+            
+                    if (!enArchivo.delete()) {
+                System.out.println("Colud not delete file");
+                return;
+            }
+
+            if (!tempFile.renameTo(enArchivo)) {
+                System.out.println("Colud not rename file");
+            }
+        }catch(FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+    }
 }
